e50b2462cf51ec2c404daacc19c58d6f5d285ecb
SmyslovSA <stepan.smyslov@gmail.com>
2018-02-06 00:02:35 +0300

    [update] fixed
    fixed small mistakes

--- Lecture_1_5_Smyslov_Stepan/Lecture_1_5/Program.cs
+++ Lecture_1_5_Smyslov_Stepan/Lecture_1_5/Program.cs
@@ -20,7 +20,7 @@ namespace Lecture_1_5
                     /* Заполняет массив от нуля до последнего индекса, после чего превращает массив в бегущую строку.
                        После окончания бегущей строки ждет её окончания в приложении и запускает цикл заново. */
                     Console.WriteLine();
-                    for (; ; ) // TODO Чисто эстетически while(true) выглядит лучше =)
+                    while (true)
                     {
                         // TODO перемудрили. Слишком много массивов и циклов =) Но да, оно работает
                         for (int i = 0; i < runningWords.Length; i++)
@@ -36,25 +36,24 @@ namespace Lecture_1_5
                             Thread.Sleep(500);
                             Console.Clear();
                         }
-                        for (int i = 1; i < runningStringArray.Length; i++)
-                        {
-                            for (int j = 1; j < runningStringArray.Length; j++)
-                                runningStringArray[j - 1] = runningStringArray[j];
-                            runningStringArray[runningStringArray.Length - i] = ' ';
-                            Console.Write(runningStringArray);
-                            Thread.Sleep(500);
-                            Console.Clear();
-                        }
+                       for (int i = 1; i < runningStringArray.Length; i++)
+                            {
+                                for (int j = 1; j < runningStringArray.Length; j++)
+                                    runningStringArray[j - 1] = runningStringArray[j];
+                                runningStringArray[runningStringArray.Length - i] = ' ';
+                                Console.Write(runningStringArray);
+                                Thread.Sleep(500);
+                                Console.Clear();
+                            }
                     }
-                    #endregion
+                #endregion
                 case ConsoleKey.D2:
                 case ConsoleKey.NumPad2:
                     #region variant2
                     // Запускает бегущую строку. После окончания сразу же её зацикливает.
                     Console.WriteLine();
-                    for (; ; )
+                    while (true)
                     {
-                        // TODO можно отлично заменить на Foreach
                         foreach (char letter in runningWords)
                         {
                             {
