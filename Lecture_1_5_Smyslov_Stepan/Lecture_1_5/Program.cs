937fcd82e22b1fbb6a06507a2955d0693724565d
Artsiom Kharytonchyk <akharytonchyk@slb.com>
2018-02-08 16:48:20 +0300

    Lecture 1-6

--- Lecture_1_5_Smyslov_Stepan/Lecture_1_5/Program.cs
+++ Lecture_1_5_Smyslov_Stepan/Lecture_1_5/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace Lecture_1_5
@@ -11,7 +7,7 @@ namespace Lecture_1_5
     {
         static void Main(string[] args)
         {
-            Console.Write("Choose variant of running string (1 or 2): ");
+            Console.Write("Choose variant of running string (1 , 2 or 3): ");
             ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
             ConsoleKey number = consoleKeyInfo.Key;
             char[] runningStringArray = new char[15];
@@ -24,29 +20,27 @@ namespace Lecture_1_5
                     /* Заполняет массив от нуля до последнего индекса, после чего превращает массив в бегущую строку.
                        После окончания бегущей строки ждет её окончания в приложении и запускает цикл заново. */
                     Console.WriteLine();
-                    for (; ; )
+                    while (true)
                     {
-                        for (int i = 0; i < runningWords.Length; i++)
+                        // DONE перемудрили. Слишком много массивов и циклов =) Но да, оно работает
+                        // Переделано с использованием substring.
+                        int j = 0;
+                        for (int i = 1; i < runningWords.Length+runningStringArray.Length; i++)
                         {
-                            if (runningStringArray.Length - i <= 0)
+                            if (i >= runningStringArray.Length && i < runningWords.Length)
                             {
-                                for (int j = 0; j < runningStringArray.Length - 1; j++)
-                                    runningStringArray[j] = runningStringArray[j + 1];
-                                runningStringArray[runningStringArray.Length - 1] = runningWords[i];
+                                Console.Write(runningWords.Substring(j, runningStringArray.Length));
+                                j++;
                             }
-                            else runningStringArray[i] = runningWords[i];
-                            Console.Write(runningStringArray);
+                            else if (i >= runningWords.Length)
+                            {
+                                Console.Write(runningWords.Substring(j));
+                                j++;
+                            }
+                            else
+                                Console.Write(runningWords.Substring(j, i)); 
                             Thread.Sleep(500);
-                            Console.Clear();
-                        }
-                        for (int i = 1; i < runningStringArray.Length; i++)
-                        {
-                            for (int j = 1; j < runningStringArray.Length; j++)
-                                runningStringArray[j - 1] = runningStringArray[j];
-                            runningStringArray[runningStringArray.Length - i] = ' ';
-                            Console.Write(runningStringArray);
-                            Thread.Sleep(500);
-                            Console.Clear();
+                            Console.Clear();                           
                         }
                     }
                     #endregion
@@ -55,21 +49,47 @@ namespace Lecture_1_5
                     #region variant2
                     // Запускает бегущую строку. После окончания сразу же её зацикливает.
                     Console.WriteLine();
-                    for (; ; )
+                    while (true)
                     {
-                        for (int i = 0; i < runningWords.Length; i++)
+                        foreach (char letter in runningWords)
                         {
                             {
                                 for (int j = 0; j < runningStringArray.Length - 1; j++)
                                     runningStringArray[j] = runningStringArray[j + 1];
-                                runningStringArray[runningStringArray.Length - 1] = runningWords[i];
+                                runningStringArray[runningStringArray.Length - 1] = letter;
                             }
                             Console.Write(runningStringArray);
                             Thread.Sleep(500);
                             Console.Clear();
                         }
                     }
-                    #endregion
+                #endregion
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    #region variant3
+                    // Вариант 1 представленный в виде работы с массивом
+                    Console.WriteLine();
+                    while (true)
+                    {
+                        for (int i = 0; i < runningWords.Length + runningStringArray.Length; i++)
+                        {
+                            if (i >= runningStringArray.Length)
+                            {
+                                for (int j = 0; j < runningStringArray.Length - 1; j++)
+                                    runningStringArray[j] = runningStringArray[j + 1];
+                                if (i<runningWords.Length)
+                                runningStringArray[runningStringArray.Length - 1] = runningWords[i];
+                                else
+                                runningStringArray[(runningStringArray.Length + runningWords.Length) - 1 - i] = ' ';
+                            }
+                            else
+                                runningStringArray[i] = runningWords[i];
+                            Console.Write(runningStringArray);
+                            Thread.Sleep(500);
+                            Console.Clear();
+                        }
+                    }
+                #endregion
                 default:
                     Console.WriteLine();
                     Console.WriteLine("You choose wrong variant");
